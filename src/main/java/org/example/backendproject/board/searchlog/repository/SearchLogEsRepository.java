8df01cc7ba7b6dcfe86e34a921acf7cb37c21ade
neisii <neisii@outlook.com>
2025-07-01 11:06:01 +0900

    11일차. ElasticSearch 기능 개선(Kafka 이벤트 추적)  - @Repository 추가

--- src/main/java/org/example/backendproject/board/searchlog/repository/SearchLogEsRepository.java
+++ src/main/java/org/example/backendproject/board/searchlog/repository/SearchLogEsRepository.java
@@ -2,7 +2,9 @@ package org.example.backendproject.board.searchlog.repository;
 
 import org.example.backendproject.board.searchlog.domain.SearchLogDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SearchLogEsRepository extends ElasticsearchRepository<SearchLogDocument, String> {
 
     // ES 저장/검색용
